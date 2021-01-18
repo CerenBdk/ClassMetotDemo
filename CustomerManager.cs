e3fd3ffa7f9118ad0f93ab8febebf19a7803416f
CerenBdk <cerenbdk@gmail.com>
2021-01-19 01:16:57 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> cList = new List<Customer> { };
+
+        public void GetCustomerList()
+        {
+            Console.WriteLine("\nList of all registered customers: ");
+            int count = 1;
+            foreach (var c in cList)
+            {
+                Console.WriteLine(count +"-"+ c.FirstName + c.LastName);
+                count++;
+            }
+            Console.WriteLine();
+        }
+
+        public void GetCustomer(Customer c)
+        {
+            Console.WriteLine("Information of customer ID {0}: ", c.CustomerID);
+            Console.WriteLine("- First Name: {1} \n- Last Name: {2} \n- Phone Number: {3} \n- Address: {4} \n- Balance: {5} \n ", 
+                c.CustomerID, 
+                c.FirstName,
+                c.LastName,
+                c.PhoneNumber,
+                c.Address,
+                c.Balance);
+        }
+
+        public void AddCutomer(Customer c)
+        {
+            cList.Add(c);
+            Console.WriteLine("{0} {1} has been added.", c.FirstName, c.LastName);
+        }
+
+        public void DeleteCustomer(Customer c)
+        {
+            cList.Remove(c);
+            Console.WriteLine("Customer ID:{0} has been deleted \n", c.CustomerID);
+            Console.WriteLine("Customer list updated.");
+            GetCustomerList();
+        }               
+    }                          
+}                              
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer 
+            { 
+                CustomerID = 1, 
+                FirstName = "Harry", 
+                LastName = "Potter", 
+                PhoneNumber = "1212121212", 
+                Address = "The Cupboard under the Stairs, + PRivet Drive, Little Whinging SURREY",
+                Balance = "£ 10.000"
+            };
+
+            Customer c2 = new Customer
+            {
+                CustomerID = 2,
+                FirstName = "Hermione",
+                LastName = "Granger",
+                PhoneNumber = "2323232323",
+                Address = "Hampstead, London.",
+                Balance = "£ 15.070"
+            };
+
+            Customer c3 = new Customer
+            {
+                CustomerID = 3,
+                FirstName = "Ron",
+                LastName = "Weasley",
+                PhoneNumber = "3434343434",
+                Address = "Ottery St. Catchpole, Devon.",
+                Balance = "£ 8.930"
+            };
+
+            Customer c4 = new Customer
+            {
+                CustomerID = 4,
+                FirstName = "Sirius",
+                LastName = "Black",
+                PhoneNumber = "4545454545",
+                Address = "12 Grimmauld Place, London.",
+                Balance = "£ 11.642"
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.AddCutomer(c1);
+            customerManager.AddCutomer(c2);
+            customerManager.AddCutomer(c3);
+            customerManager.AddCutomer(c4);
+
+            customerManager.GetCustomerList();
+            customerManager.GetCustomer(c1);
+            customerManager.DeleteCustomer(c3);
+            
+            Console.ReadLine();
+        }
+    }
+}
